15f93d7b7d55b5a4268bf6e49251bd82823bb46a
Aymeric Bethencourt <aymeric.bethencourt@gmail.com>
2020-03-24 15:07:29 +0100

    Correction ex5

--- TP1-Solidity/4-MyToken/MyToken_test.sol
+++ TP1-Solidity/4-MyToken/MyToken_test.sol
@@ -9,14 +9,14 @@ contract MyTokenTest {
 
     /// #sender: account-0
     function beforeAll() public {
-        myToken = new ERC20("YOLO Coin", "YOLO", 1000);
+        myToken = new MyToken("YOLO Coin", "YOLO", 1000);
     }
 
     /// #sender: account-0
     function checkReceiverGotTheTokens() public {
         myToken.transfer(TestsAccounts.getAccount(1), 200);
         Assert.equal(
-            ERC20ToTest.getBalanceOf(TestsAccounts.getAccount(1)),
+            myToken.balanceOf(TestsAccounts.getAccount(1)),
             200,
             "wrong value"
         );
